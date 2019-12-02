60d15674712574d775c772be97052b2dd50986f1
dpereskokov <dmitry.v.pereskokov@gmail.com>
2019-12-02 16:02:26 +0800

    adapt constructor name TrustWeb3Provider

--- js/src/index.ts
+++ js/src/index.ts
@@ -4,6 +4,7 @@
 import { WalletLink } from "./WalletLink"
 import { WalletLinkProvider } from "./WalletLinkProvider"
 import {TrustRelay} from "./TrustRelay";
+import {TrustWeb3Provider} from "./TrustWeb3Provider";
 
 export { WalletLink } from "./WalletLink"
 export { WalletLinkProvider } from "./WalletLinkProvider"
@@ -14,6 +15,7 @@ declare global {
     WalletLink: typeof WalletLink
     WalletLinkProvider: typeof WalletLinkProvider
     TrustRelay: typeof TrustRelay
+    TrustWeb3Provider: typeof TrustWeb3Provider
     ethereum?: WalletLinkProvider
     webkit: any
     trustMessage: (...args: any) => void
@@ -21,7 +23,8 @@ declare global {
 }
 
 if (typeof window !== "undefined") {
-  window.WalletLink = WalletLink
-  window.WalletLinkProvider = WalletLinkProvider
-  window.TrustRelay = TrustRelay
+  window.WalletLink = WalletLink;
+  window.WalletLinkProvider = WalletLinkProvider;
+  window.TrustRelay = TrustRelay;
+  window.TrustWeb3Provider = TrustWeb3Provider;
 }
